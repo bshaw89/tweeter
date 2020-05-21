1eedffdc85028687edbb5e56960f4bf76119e77b
Ben S <shaw.j.benjamin@gmail.com>
2020-05-21 00:55:51 +0000

    Refactor code

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -7,10 +7,12 @@ $(document).ready(function() {
     $(this).closest("form").find(".counter").val(140 - $(this).val().length);
 
     if ($(this).closest("form").find(".counter").val() <= 0) {
-      $(this).closest("form").find(".counter").css("color", "red");
+      $(this).closest("form").find(".counter").css("color", "red"); // append class .class
     } else {
-      $(this).closest("form").find(".counter").css("color", "#545149")
+      $(this).closest("form").find(".counter").css("color", "#545149") // .remove class (class name)
     }
   });
 
 });
+
+// class: error, color: red
