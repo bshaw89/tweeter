e57d330c49ae4ede2fb63a708b4b3cc989e7cf72
Ben S <shaw.j.benjamin@gmail.com>
2020-05-23 19:41:52 +0000

    Code cleanup

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,35 +9,30 @@ $(() => {
   * jQuery is already loaded
   * Reminder: Use (and do all your DOM work in) jQuery's document ready function
   */
-
+  
+  // Hidden elements on page load. Click to reveal.
   $( "#new-tweet-icon" ).click(function() {
     $(".container form").fadeToggle();
     $("#tweet-text").focus();
-    // $(".container from").animate({
-    //   height: 10%
-    // }, function() {
-      
-    // });
-  })
-
+  });
 
   const renderTweets = function(tweetsArr) {
     let tweetsArrRev = tweetsArr.reverse();
     for (let tweet of tweetsArrRev) {
       let output = createTweetElement(tweet);
       $(`.tweets-all`).append(output);
-     }
     }
+  };
 
-    const escape = function(str) {
-      let div = document.createElement('div');
-      div.appendChild(document.createTextNode(str));
-      return div.innerHTML;
-    }
+  const escape = function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
- const createTweetElement = function(tweet) {
+  const createTweetElement = function(tweet) {
 
- let $output = `<article>
+    let $output = `<article>
  <header>
   <div id="handle">
   <span id="avatar">
@@ -58,117 +53,57 @@ $(() => {
   <i class="fas fa-heart"></i>
   </span>
 </footer>
-</article>`
+</article>`;
 
-return $output;
-}
+    return $output;
+  };
 
-// Date.now() -- current timestamp
-// (created_at) - (above^)
+  const timeStamp = function(date) {
+    const currentTime = Date.now();
+    const timeDelta = currentTime - date;
+    if (timeDelta < 60000) {
+      return "Just now";
+    } else if (timeDelta < 60000 * 30) {
+      return `${Math.round(timeDelta / 1000 / 60)} minutes ago`;
+    } else if (timeDelta < 60000 * 60 * 10) {
+      return `${Math.round(timeDelta / 1000 / 60 / 60)} hours ago`;
+    } else if (timeDelta < 60000 * 60 * 24 * 30) {
+      return `${Math.round(timeDelta / 1000 / 60 / 60 / 24)} days ago`;
+    } else {
+      return `a long time ago`;
+    }
+  };
 
-// function takes in date
+  // submit new tweet form
+  $( ".new-tweet form" ).submit(function(event) {
+    event.preventDefault();
+    const rawText = $('#tweet-text').val();
+    const output = $(this).serialize();
+    if (rawText === "") {
+      $("#error-msg p").text("Say something!");
+      $( "#error-msg" ).fadeIn().show();
+    } else if (rawText.length > 140) {
+      $("#error-msg p").text("Too verbose!");
+      $( "#error-msg" ).show();
+    } else {
+      $( "#error-msg" ).fadeOut().hide();
+      $.ajax({
+        method: "POST",
+        url: "/tweets",
+        data: output
+      })
+        .done(function() {
+          $('.tweets-all').empty();
+          $(loadTweets());
+          $(".new-tweet form").trigger("reset");
+        });
+    }
+  });
 
-const timeStamp = function(date) {
-  const currentTime = Date.now();
-  const timeDelta = currentTime - date;
-  if (timeDelta < 60000) {
-    return "Just now";
-  } else if (timeDelta < 60000*30) {
-    return `${Math.round(timeDelta / 1000 / 60)} minutes ago`; 
-  } else if (timeDelta < 60000 * 60 * 10) {
-    return `${Math.round(timeDelta / 1000 / 60 / 60)} hours ago`
-  } else if (timeDelta < 60000 * 60 * 24 * 30) {
-    return `${Math.round(timeDelta / 1000 / 60 / 60 / 24)} days ago`
-  } else {
-    return `a long time ago`;
-  }
-  // days ago
-  // hours ago
-  // minutes ago
-  // just now
-}
-
-
-//  const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1589749532000
-//   }
-// ]
-
-$( ".new-tweet form" ).submit(function ( event ) {
-  event.preventDefault();
-  // event.target.value
-  const rawText = $('#tweet-text').val();
-  const output = $(this).serialize()
-  // console.log(output)
-  // console.log($("#error-msg p").text("hiii"));
-  if (rawText === "") {
-    $("#error-msg p").text("Say something!")
-    $( "#error-msg" ).fadeIn().show();
-  } else if (rawText.length > 140) {
-    $("#error-msg p").text("Too verbose!")
-    $( "#error-msg" ).show();
-  } else {
-    $( "#error-msg" ).fadeOut().hide();
-    $.ajax({
-      method: "POST",
-      url: "/tweets",
-      // data: { text: "text" }
-      data: output
-    })
-    .done(function (data) {
-      // console.log(data);
-      $('.tweets-all').empty();
-      // reset form jQuery method
-      // $("textarea").val("");
-      // $("counter").val(140);
-      $(loadTweets());
-      $(".new-tweet form").trigger("reset");
-    })
-  }
-})
-
-
-// $.ajax('index.html', { method: 'POST' }, data: { })
-//     .then(function (morePostsHtml) {
-//       console.log('Success: ', morePostsHtml);
-//       $button.replaceWith(morePostsHtml);
-//     });
-
-const loadTweets = function() {
-  $.ajax('/tweets', { method: 'GET' })
-  .then(function (tweetsJSON) {
-    console.log("Success: ", renderTweets(tweetsJSON));
-  })
-}
-// loadTweets();
-
-//  renderTweets(data);
-
-
- 
+  const loadTweets = function() {
+    $.ajax('/tweets', { method: 'GET' })
+      .then(function(tweetsJSON) {
+        console.log("Success: ", renderTweets(tweetsJSON));
+      });
+  };
 });
-
-// :nth-child won't work because more tweets keep getting added
-// currently adds random user when posting new tweet; need to refresh page to see
-// add slide animation to error message
-// switch px to em
--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -1,18 +1,14 @@
 $(document).ready(function() {
   // --- our code goes here ---
   $("textarea").keyup(function() {
-    // console.log(140 - $(this).val().length);
-    // console.log($(this))
     console.log($(this).closest("form").find(".counter").val());
     $(this).closest("form").find(".counter").val(140 - $(this).val().length);
 
     if ($(this).closest("form").find(".counter").val() <= 0) {
-      $(this).closest("form").find(".counter").css("color", "red"); // append class .class
+      $(this).closest("form").find(".counter").css("color", "red");
     } else {
-      $(this).closest("form").find(".counter").css("color", "#545149") // .remove class (class name)
+      $(this).closest("form").find(".counter").css("color", "#545149");
     }
   });
 
 });
-
-// class: error, color: red
